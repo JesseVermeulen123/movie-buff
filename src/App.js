0a15ecebd20b3bddd75fe0d09ccd5f69fb46a961
OliverVermeulen <72196313+OliverVermeulen@users.noreply.github.com>
2023-02-28 15:31:57 +0200

    Added local storage persistence

--- src/App.js
+++ src/App.js
@@ -15,10 +15,8 @@ function App() {
 
   const getMovieRequest = async () => {
     const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=6993a6e4`;
-
     const response = await fetch(url);
     const responseJson = await response.json();
-
     if (responseJson.Search) {
       setMovies(responseJson.Search);
     }
@@ -28,26 +26,43 @@ function App() {
     getMovieRequest(searchValue);
   }, [searchValue]);
 
+  useEffect(() => {
+    const movieFavourites = JSON.parse(
+      localStorage.getItem("react-movie-app-favourites")
+    );
+    setFavourites(movieFavourites);
+  }, []);
+
+  // Save Favourites to local storage
+  const saveToLocalStorage = (items) => {
+    localStorage.setItem("react-movie-app-favourites", JSON.stringify(items));
+  };
+
+  // Add movie to Favourites
   const addFavouriteMovie = (movie) => {
     const newFavouriteList = [...favourites, movie];
     setFavourites(newFavouriteList);
+    saveToLocalStorage(newFavouriteList);
   };
 
+  // Remove movie from Favourites
   const removeFavouriteMovie = (movie) => {
     const newFavouriteList = favourites.filter(
       (favourite) => favourite.imdbID !== movie.imdbID
     );
-
-    setFavourites(newFavouriteList)
+    setFavourites(newFavouriteList);
+    saveToLocalStorage(newFavouriteList);
   };
 
   return (
     <div className="container-fluid movie-app">
+      {/* Movies Heading and Search box */}
       <div className="row d-flex align-items-center mt-4 mb-4">
         <MovieListHeading heading="Movies" />
         <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
       </div>
 
+      {/* Movie List */}
       <div className="row">
         <MovieList
           movies={movies}
@@ -56,10 +71,12 @@ function App() {
         />
       </div>
 
+      {/* Favourites Heading */}
       <div className="row d-flex align-items-center mt-4 mb-4">
         <MovieListHeading heading="Favourites" />
       </div>
 
+      {/* Favourites List */}
       <div className="row">
         <MovieList
           movies={favourites}
